606c3f9683f7909acedb20141e73faf58e59dd0b
Sergey <ssnovak@stud.kpfu.ru>
2023-08-25 13:59:30 +0300

    + Base Model

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
 
 
 @app.get("/")
@@ -12,3 +19,8 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, "item_name": item.name, "item_price": item.price}
