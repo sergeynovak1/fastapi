f8912025ac0c46bfe8cdaca921877b6ae7376efd
Sergey <ssnovak@stud.kpfu.ru>
2023-08-30 13:03:53 +0300

    + datetime + uuid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from typing import Optional, List, Set, Dict
+from datetime import datetime, time, timedelta
+from uuid import UUID
 from fastapi import FastAPI, Path, Query, Body
 from pydantic import BaseModel, Field, HttpUrl
 from enum import Enum
@@ -104,6 +106,27 @@ async def get_model(model_name: ModelName):
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+
+@app.put("/items1/{item_id}")
+async def read_items(
+        item_id: UUID,
+        start_datetime: Optional[datetime] = Body(None),
+        end_datetime: Optional[datetime] = Body(None),
+        repeat_at: Optional[time] = Body(None),
+        process_after: Optional[timedelta] = Body(None),
+):
+    start_process = start_datetime + process_after
+    duration = end_datetime - start_process
+    return {
+        "item_id": item_id,
+        "start_datetime": start_datetime,
+        "end_datetime": end_datetime,
+        "repeat_at": repeat_at,
+        "process_after": process_after,
+        "start_process": start_process,
+        "duration": duration,
+    }
 
 
 @app.get("/items1/{item_id}")
