6453954d87ab12f29e3912eafc6025382d4c2420
Sergey <ssnovak@stud.kpfu.ru>
2023-08-30 09:17:14 +0300

    + deprecated

--- main.py
+++ main.py
@@ -36,7 +36,19 @@ async def read_item(
 
 
 @app.get("/items/")
-async def read_items(q: Optional[str] = Query(None, alias="item-query")):
+async def read_items(
+        q: Optional[str] = Query(
+        None,
+        alias="item-query",
+        title="Query string",
+        description="Query string for the items to search in the database that have a good match",
+        min_length=3,
+        max_length=50,
+        # regex="^fixedquery$",
+        deprecated=True,
+        )
+
+):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
