52d3560f963e044ac9aed58cfeecdafea321c5f1
Sergey <ssnovak@stud.kpfu.ru>
2023-08-30 11:22:25 +0300

    + pydantic models

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-from typing import Optional
+from typing import Optional, List, Set
 from fastapi import FastAPI, Path, Query, Body
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, HttpUrl
 from enum import Enum
 
 app = FastAPI()
 
 
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
+
+
 class Item(BaseModel):
     name: str
-    description: Optional[str] = None
-    price: float
+    description: Optional[str] = Field(
+        None, title="The description of the item", max_length=300
+    )
+    price: float = Field(..., gt=0, description="The price must be greater than zero")
     tax: Optional[float] = None
+    tags: Set[str] = set()
+    images: Optional[List[Image]] = None
 
 
 class User(BaseModel):
