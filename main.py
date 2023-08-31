9c79ef7e5e6e400d485b07f9845d013d29efedaa
Sergey <ssnovak@stud.kpfu.ru>
2023-08-31 12:56:47 +0300

    + depends

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import Optional, List, Set, Dict
 from datetime import datetime, time, timedelta
 from uuid import UUID
-from fastapi import FastAPI, Path, Query, Body, Cookie, Header, status, Form, UploadFile, File, Request
+from fastapi import FastAPI, Path, Query, Body, Cookie, Header, status, Form, UploadFile, File, Request, Depends
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel, Field, HttpUrl
@@ -240,3 +240,31 @@ async def update_item(item_id: str, item: Item2):
     updated_item = stored_item_model.copy(update=update_data)
     items[item_id] = jsonable_encoder(updated_item)
     return jsonable_encoder(updated_item)
+
+
+async def common_parameters(q: Optional[str] = None, skip: int = 0, limit: int= 100):
+    return {"q": q, "skip": skip, "limit": limit}
+
+
+@app.get("/users/")
+async def read_users(commons: dict = Depends(common_parameters)):
+    return commons
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+class CommonQueryParams:
+    def __init__(self, q: Optional[str] = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
+
+@app.get("/items3/")
+async def read_items(commons: CommonQueryParams = Depends(CommonQueryParams)):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+        items = fake_items_db[commons.skip : commons.skip + commons.limit]
+        response.update({"items": items})
+    return response
