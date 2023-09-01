15bd17d9b01aec7256deb965a6335132ec2d8f9b
Sergey <ssnovak@stud.kpfu.ru>
2023-09-01 13:37:15 +0300

    + update url

--- app/main.py
+++ app/main.py
@@ -4,7 +4,22 @@ from .dependencies import get_query_token, get_token_header
 from .internal import admin
 from .routers import items, users
 
-app = FastAPI(dependencies=[Depends(get_query_token)])
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "items",
+        "description": "Manage items. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+]
+
+app = FastAPI(dependencies=[Depends(get_query_token)], openapi_tags=tags_metadata, docs_url="/documentation", redoc_url=None)
 
 
 app.include_router(users.router)
