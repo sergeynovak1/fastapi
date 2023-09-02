129205261747ba6a0563dfdd21194e638067079d
Sergey <ssnovak@stud.kpfu.ru>
2023-09-02 12:57:45 +0300

    + update status code

--- main_base.py
+++ main_base.py
@@ -343,3 +343,14 @@ def get_headers():
     content = {"message": "Hello World"}
     headers = {"X-Cat-Dog": "alone in the world", "Content-Language": "en-US"}
     return JSONResponse(content=content, headers=headers)
+
+
+tasks = {"foo": "Listen to the Bar Fighters"}
+
+
+@app.put("/get-or-create-task/{task_id}", status_code=200)
+def get_or_create_task(task_id: str, response: Response):
+    if task_id not in tasks:
+        tasks[task_id] = "This didn't exist before"
+        response.status_code = status.HTTP_201_CREATED
+    return tasks[task_id]
