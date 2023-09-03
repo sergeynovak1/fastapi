85b186f2859c5f4d8e430933d110d2e76124080d
Sergey <ssnovak@stud.kpfu.ru>
2023-09-03 12:23:02 +0300

    + check password in simple base auth

--- main_oauth_basic.py
+++ main_oauth_basic.py
@@ -1,4 +1,6 @@
-from fastapi import Depends, FastAPI
+import secrets
+
+from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
@@ -6,6 +8,18 @@ app = FastAPI()
 security = HTTPBasic()
 
 
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username,"stanleyjobson")
+    correct_password = secrets.compare_digest(credentials.password,"swordfish")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+
 @app.get("/users/me")
-def read_current_user(credentials: HTTPBasicCredentials = Depends(security)):
-    return {"username": credentials.username, "password": credentials.password}
+def read_current_user(username: str = Depends(get_current_username)):
+    return {"username": username}
